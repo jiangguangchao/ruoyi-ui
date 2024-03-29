90de048fa8eeb04efe30369e19c2ad8e4ac11b4a
jgc <jiangguangchao157@163.com>
2024-03-29 23:43:51 +0800

    排班页面

---
+++ src/api/fl/schedule.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询排班列表
+export function listSchedule(query) {
+  return request({
+    url: '/fl/schedule/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询排班详细
+export function getSchedule(id) {
+  return request({
+    url: '/fl/schedule/' + id,
+    method: 'get'
+  })
+}
+
+// 新增排班
+export function addSchedule(data) {
+  return request({
+    url: '/fl/schedule',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改排班
+export function updateSchedule(data) {
+  return request({
+    url: '/fl/schedule',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除排班
+export function delSchedule(id) {
+  return request({
+    url: '/fl/schedule/' + id,
+    method: 'delete'
+  })
+}
