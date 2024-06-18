6252988cc0b510eb28b8f929b2193dc8c2f45346
jgc_work <jiangguangchao157@163.com>
2024-06-18 18:01:04 +0800

    工作量显示

--- src/views/fl/workload/index.vue
+++ src/views/fl/workload/index.vue
@@ -131,7 +131,7 @@ export default {
       },
       // 表单参数
       form: {},
-      showSimpleInfo: true,
+      showSimpleInfo: false,
       dateRange: [],
       workloadList: [],
       workloadStatList: [],
